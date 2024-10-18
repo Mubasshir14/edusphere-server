b5744ea464b163990746c4dddb4e0e631e6ae477
Mubasshir Al Kasshaf <mubasshiralkasshaf22@gmail.com>
2024-10-18 16:48:40 +0600

    MongoDb

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3aom8f0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
+
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
